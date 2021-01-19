52c0b485695ae807f73bb1230dfd75d2e5ae295f
Evgenii <evtatdevelop@gmail.com>
2021-01-19 15:45:39 +0700

    add some data transformations. city list

--- AviaTickets/src/js/app.js
+++ AviaTickets/src/js/app.js
@@ -2,8 +2,23 @@ import "../css/rest.css";
 import "../css/style.css";
 import locations from "./store/locations";
 
+const originDatalist = document.querySelector("#originList");
+const distinationList = document.querySelector("#distinationList");
+let options = "";
+
 locations.init().then((res) => {
-  console.log(res);
+  // console.log(res);
   console.log(locations);
-  console.log(locations.getCitiesByCountryCode("TH"));
+
+  // console.log(locations.getCitiesByCountryCode("TH"));
 });
+
+function renderDatalist(data, aim) {
+  const fragment = document.createDocumentFragment();
+  // data.forEach((element) => {
+  //   const optionEl = document.createElement("option");
+  //   optionEl.textContent = element.name;
+  //   fragment.append(optionEl);
+  // });
+  aim.append(fragment);
+}
--- AviaTickets/src/js/store/locations.js
+++ AviaTickets/src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
     this.api = api;
     this.countries = null;
     this.cities = null;
+    this.shortCitiesList = null;
   }
   async init() {
     const response = await Promise.all([
@@ -13,10 +14,39 @@ class Locations {
     ]);
 
     const [countries, cities] = response;
-    this.countries = countries;
-    this.cities = cities;
-
+    // this.countries = countries;
+    this.countries = this.serializeCountries(countries);
+    this.cities = this.serializeCities(cities);
+    this.shortCitiesList = this.createShortCitiesList(this.cities);
     return response;
+  }
+
+  createShortCitiesList(cities) {
+    return Object.entries(cities).reduce((acc, [key]) => {
+      acc[key] = null;
+      return acc;
+    }, {});
+  }
+
+  serializeCountries(countries) {
+    return countries.reduce((acc, country) => {
+      acc[country.code] = country;
+      return acc;
+    }, {});
+  }
+
+  serializeCities(cities) {
+    return cities.reduce((acc, city) => {
+      const countyName = this.getCountryNameByCode(city.country_code);
+      const cityName = city.name || name_translations.en;
+      const key = `${cityName}, ${countyName}`;
+      acc[key] = city;
+      return acc;
+    }, {});
+  }
+
+  getCountryNameByCode(code) {
+    return this.countries[code].name;
   }
 
   getCitiesByCountryCode(code) {
