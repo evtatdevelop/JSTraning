c1ae0ca6b5c1411caaf2c0dfb54358840c5fc978
Evgenii <evtatdevelop@gmail.com>
2021-02-10 11:20:30 +0700

    steTimeout && setTimeinterval

---
+++ ClearJSLearn/src/js/settimeout.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log('Advanced work with functions 🛠');
+console.log('setTimeout && setInterval ⏰');
+
+// function printNumbers(from, to) {
+//   let timer = setTimeout(function run() {
+//     console.log(from++);
+
+//     timer = setTimeout(run, 1000);
+//     if (from > +to ) clearTimeout(timer);
+//   }, 1000);
+// }
+
+// function printNumbers1(from, to) {
+//   const timer = setInterval(() => {
+//     console.log(from++);
+//     if (from > +to ) clearInterval(timer);
+//   }, 1000);
+// }
+
+// function printNumbers2(from, to) {
+//   function run() {
+//     console.log(from++);
+//     if (from > to ) clearInterval(timer);
+//   }
+//   run();
+//   const timer = setInterval(run, 1000);
+// }
+
+// printNumbers(2, 7);
+// printNumbers1(2, 7);
+// printNumbers2(2, 7);
