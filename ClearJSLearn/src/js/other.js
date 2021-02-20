824a3a7b6d3d38ec5f67cf4884b7e9ef819080f5
Evgenii <evtatdevelop@gmail.com>
2021-02-20 11:50:35 +0700

    Internationalization

---
+++ ClearJSLearn/src/js/other.js
@@ -0,0 +1,54 @@
+'use strict';
+
+console.log('Other 🧿');
+
+// function evalCalc() {
+//   const request = prompt('Your request?');
+//   alert(eval(request));
+// }
+// evalCalc();
+
+/**
+ *
+ */
+// const date = new Date(2021, 1, 20, 11, 45, 0);
+
+// const formatter = new Intl.DateTimeFormat('ru', {
+//   weekday: 'long',
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+// });
+// alert( formatter.format(date) ); // 31.12.2014
+
+// const formatter1 = new Intl.DateTimeFormat('us-US', {
+//   weekday: 'long',
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+// });
+// alert( formatter1.format(date) ); // 12/31/2014
+
+// const formatter2 = new Intl.DateTimeFormat('th-TH', {
+//   weekday: 'long',
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+// });
+// alert( formatter2.format(date) ); // 12/31/2014
+
+/**
+ *
+ */
+// const formatter = new Intl.NumberFormat('ru', {
+//   style: 'currency',
+//   currency: 'USD',
+//   minimumFractionDigits: 2,
+// });
+// alert( formatter.format(1234.5) ); // 1 234,50 £
+// const formatter1 = new Intl.NumberFormat('us', {
+//   style: 'currency',
+//   currency: 'USD',
+//   minimumFractionDigits: 2,
+// });
+// alert( formatter1.format(1234.5) ); // 1 234,50 £
