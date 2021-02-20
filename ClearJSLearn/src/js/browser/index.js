1750fb22e74b2ebc3157cc96697568512a491749
Evgenii <evtatdevelop@gmail.com>
2021-02-20 19:31:54 +0700

    Dom novigation

---
+++ ClearJSLearn/src/js/browser/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+console.log('browser 🎀');
+
+// function sayHi() {
+//   console.log('H!');
+// }
+// sayHi();
+// window.sayHi();
+
+// console.log(window.innerHeight);
+
+// document.body.style.background = '#161619';
+// setTimeout(() => document.body.style.background = '', 1000);
+
+/**
+ *
+ */
+// const app = document.body.firstElementChild;
+// const section = app.firstElementChild.nextElementSibling;
+// const h2 = section.firstElementChild;
+// const ul = h2.nextElementSibling;
+// const liPite = ul.lastElementChild;
+// console.log(h2, liPite);
+
+/**
+ *
+ */
+// const table = document.createElement('table');
+// for (let i = 1; i <= 5; i++) {
+//   const tr = document.createElement('tr');
+//   for (let j = 1; j <= 5; j++ ) {
+//     const td = document.createElement('td');
+//     td.textContent = `${j}:${i}`;
+//     tr.append(td);
+//   }
+//   table.append(tr);
+//   table.classList.add('table');
+// }
+// section.append(table);
+
+// const tableElement = section.lastElementChild;
+// const tabElRows = tableElement.rows;
+// for (const row of tabElRows) {
+//   const rowCells = row.cells;
+//   for (const cell of rowCells) {
+//     if (row.rowIndex === cell.cellIndex) {
+//       cell.style.backgroundColor = 'red';
+//     }
+//   }
+// }
