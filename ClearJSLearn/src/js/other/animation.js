88f9cbd78b4e37a79ae4cd576aca07a14c74ff68
Evgenii <evtatdevelop@gmail.com>
2021-04-24 21:11:11 +0700

    JS Animation Simple Ball

--- ClearJSLearn/src/js/other/animation.js
+++ ClearJSLearn/src/js/other/animation.js
@@ -47,3 +47,55 @@ console.log('Animation 🎭');
 //   });
 // }
 // showCircle(200, 200, 150);
+
+/**
+ * 
+ */
+const animationBox = document.createElement('div');
+animationBox.style.position = 'relative';
+const ball = document.createElement('img');
+animationBox.append(ball);
+ball.src = 'img/svg/ball.svg';
+ball.style.position = 'absolute';
+document.body.append(animationBox);
+
+function animate(options) {
+  var start = performance.now();
+  requestAnimationFrame(function animate(time) {
+    // timeFraction от 0 до 1
+    var timeFraction = (time - start) / options.duration;
+    if (timeFraction > 1) timeFraction = 1;
+    // текущее состояние анимации
+    var progress = options.timing(timeFraction)    
+    options.draw(progress);
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+}
+
+function makeEaseOut(timing) {
+  return function(timeFraction) {
+    return 1 - timing(1 - timeFraction);
+  }
+}
+
+function bounce(timeFraction) {
+  for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+    if (timeFraction >= (7 - 4 * a) / 11) {
+      return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+    }
+  }
+}
+
+let bounceEaseOut = makeEaseOut(bounce);
+
+ball.onclick = function() {
+  animate({
+    duration: 3000,
+    timing: bounceEaseOut,
+    draw: function(progress) {
+      ball.style.top = progress * 400 + 'px';
+    }
+  });
+};
