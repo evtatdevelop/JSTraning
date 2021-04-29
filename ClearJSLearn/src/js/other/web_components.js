e09d6ba11c8f72935bb381049a5ea3b2f7725cdb
Evgenii <evtatdevelop@gmail.com>
2021-04-29 09:57:38 +0700

    Web-components, Custom Elements, Shadow DOM, template

--- ClearJSLearn/src/js/other/web_components.js
+++ ClearJSLearn/src/js/other/web_components.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log('Web-components ⛓');
+
+// const liveTm = document.createElement('live-timer');
+// liveTm.addEventListener('tick', (e) => {
+//   console.log(e.detail);
+// });
+// document.body.append(liveTm);
